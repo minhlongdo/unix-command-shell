9dc804a4fc4d321d07bf3ff917b4136ac8bfbead
Minh-Long Do <minhlong94@yahoo.de>
2014-10-13 21:13:14 +0100

    Create window.

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include "init/init.h"
-#include "util/util.h"
-#include "datastructure/env.h"
+#include <gtk/gtk.h>
 
 int main(int argc, char** argv) {
+  GtkWidget *terminal;
+
+  gtk_init(&argc, &argv);
+
+  terminal = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_widget_show(terminal);
+  gtk_main();
+
   return 0;
 }
