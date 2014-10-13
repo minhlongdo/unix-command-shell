9edd507fd9b8d8f8485a67369d66831d65894f7d
Minh-Long Do <minhlong94@yahoo.de>
2014-10-13 23:47:38 +0100

    Able to read environment variable.

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include "init/init.h"
+#include "util/util.h"
+#include "datastructure/env.h"
 #include <gtk/gtk.h>
 
 int main(int argc, char** argv) {
+  /*
   GtkWidget *terminal;
 
   gtk_init(&argc, &argv);
@@ -9,6 +12,11 @@ int main(int argc, char** argv) {
   terminal = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   gtk_widget_show(terminal);
   gtk_main();
+  */
+
+
+  printf("Starting Unix Command Shell.\n");
+  read_env_val("profile");
 
   return 0;
 }
