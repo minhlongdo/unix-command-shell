4bde1d504e30aef351f6c7d18252cf9fde294a86
Minh-Long Do <minhlong94@yahoo.de>
2014-10-14 00:22:40 +0100

    Replace string function working.

--- util/util.c
+++ util/util.c
@@ -1,6 +1,6 @@
 #include "util.h"
 
-void read_env_val(const char* profile) {
+void read_env_val(const char* profile, EnvVariable** env) {
   FILE *infile;
   char line_buffer[BUFSIZ]; /* BUFSIZ is defined if you include stdio.h */
   char line_number;
@@ -10,9 +10,36 @@ void read_env_val(const char* profile) {
   }
 
   while (fgets(line_buffer, sizeof(line_buffer), infile)) {
-    if(strstr(line_buffer,"HOME") != NULL) {
+    if(strstr(line_buffer,"HOME") != NULL) {\
+      char* tmp;
+      replace(line_buffer, "HOME=", &tmp);
+      printf("%s",tmp);
     }
     else if(strstr(line_buffer,"PATH") != NULL) {
+      char* tmp;
+      replace(line_buffer, "PATH=", &tmp);
+      printf("%s",tmp);
     }
   }
+}
+
+void replace(char* buffer, char* string, char **result) {
+  int len_buffer = 0;
+  int len_string = 0;
+  while(buffer[len_buffer] != '\0') {
+    ++len_buffer;
+  }
+  while(string[len_string] != '\0') {
+    ++len_string;
+  }
+  printf("Buffer: %s | %i\n", buffer, len_buffer);
+  printf("Replacement: %s | %i\n", string, len_string);
+  int index = 0;
+  int i = 0;
+  char *tmp = malloc(sizeof(char)*(len_buffer - len_string));
+  for(i=len_string;i<len_buffer;i++) {
+    tmp[index] = buffer[i];
+    index++;
+  }
+  *result = tmp;
 }
--- util/util.h
+++ util/util.h
@@ -4,8 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "../datastructure/env.h"
 
 /* Open existing file for reading */
-void read_env_val(const char* profile); /* Find environment variable */
+void read_env_val(const char* profile, EnvVariable** env); 					 /* Find environment variable */
+void replace(char* buffer, char* string, char** result); 						 /* Replace string */
 
 #endif
