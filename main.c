b6663f5ee7b2a09fe49ff7b542ca0f0ca193e263
Minh-Long Do <minhlong94@yahoo.de>
2014-10-14 11:05:38 +0100

    Remove comments.

--- main.c
+++ main.c
@@ -1,18 +1,8 @@
 #include "init/init.h"
 #include "util/util.h"
 #include "datastructure/env.h"
-#include <gtk/gtk.h>
 
 int main(int argc, char** argv) {
-  /*
-  GtkWidget *terminal;
-
-  gtk_init(&argc, &argv);
-
-  terminal = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-  gtk_widget_show(terminal);
-  gtk_main();
-  */
 
   EnvVariable* env = (EnvVariable*)malloc(sizeof(EnvVariable));
   printf("Starting Unix Command Shell.\n");
