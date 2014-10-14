5203f0b324812e12141f022525a7a104ee02d50a
Minh-Long Do <minhlong94@yahoo.de>
2014-10-14 11:15:04 +0100

    Remove comment.

--- util/util.h
+++ util/util.h
@@ -6,7 +6,6 @@
 #include <string.h>
 #include "../datastructure/env.h"
 
-/* Open existing file for reading */
 void read_env_val(const char* profile, EnvVariable** env); 					 /* Find environment variable */
 void replace(char* buffer, char* string, char** result); 						 /* Replace string */
 
