8727b167e8ca6662d6973f3c26085541b4d41585
Minh-Long Do <minhlong94@yahoo.de>
2014-10-22 22:20:36 +0100

    Working command input.

--- main.c
+++ main.c
@@ -4,13 +4,22 @@
 int main(int argc, char** argv) {
 
   EnvVariable* env = (EnvVariable*)malloc(sizeof(EnvVariable));
+  char* cmd = (char*)malloc(sizeof(char)*256);
+  char* dir = (char*)malloc(sizeof(char)*1024);
   init(&env);
 
-  printf("PATH: %s", env->path);
-  printf("HOME: %s", env->home);
+  while(1) {
+    get_current_dir(&dir);
+    printf("%s>", dir);
+    fgets(cmd, 256, stdin);
+
+    memset(dir, '\0', strlen(dir));
+    memset(cmd, '\0', strlen(cmd));
+  }
 
   /* Free memory */
   free(env);
+  free(cmd);
 
   return 0;
 }
