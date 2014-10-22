520f275cdd9fd506e22dade212d1f8f240723694
Minh-Long Do <minhlong94@yahoo.de>
2014-10-22 14:53:23 +0100

    Commented function properly.

--- init/init.c
+++ init/init.c
@@ -1,5 +1,11 @@
 #include "init.h"
 
+/**
+ * Initialize necessary data for unix command shell.
+ *
+ * @param env Stores environment variables value from file profile.
+ * @return
+ */
 void init(EnvVariable** env) {
   read_env_val("profile", &(*env));
 }
