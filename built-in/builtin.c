389c1655a6d4aba21af60e7c4f33d9fc680fb5ef
Minh-Long Do <minhlong94@yahoo.de>
2014-10-23 03:02:40 +0100

    Move builtin cd to builtin folder.

---
+++ built-in/builtin.c
@@ -0,0 +1,37 @@
+#include "builtin.h"
+
+/**
+* Change directory.
+*
+* @param dir Directory to change to.
+* @return
+*/
+void change_dir(char** dir, EnvVariable** env) {
+	char* debug = (char*)malloc(sizeof(char)*1024);
+	get_current_dir(&debug);
+	printf("Current dir: %s\n", debug);
+
+	int flag = -1;
+	if((*dir) == NULL) {
+		flag = chdir((*env)->home);
+		if (flag == -1) {
+			fprintf(stderr, "directory %s does not exist.", (*env)->home);
+		} else {
+			memset(debug, '\0', 1024);
+			printf("Directory change successful.\n");
+			get_current_dir(&debug);
+			printf("Changed dir: %s\n", debug);
+		}
+	} else {
+		flag = chdir((*dir));
+		if (flag == -1) {
+			fprintf(stderr, "directory %s does not exist.", (*dir));
+		} else {
+			memset(debug, '\0', 1024);
+			printf("Directory change successful.\n");
+			get_current_dir(&debug);
+			printf("Changed dir: %s\n", debug);
+		}
+	}
+	free(debug);
+}
---
+++ built-in/builtin.h
@@ -0,0 +1,7 @@
+#ifndef _BUILTIN_H
+#define _BUILTIN_H
+
+/* Change directory */
+void change_dir(char** dir, EnvVariable** env);
+
+#endif
