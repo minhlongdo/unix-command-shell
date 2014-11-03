ca386e64078634669c37ff3ab8fe4002226bd374
Minh-Long Do <minhlong94@yahoo.de>
2014-11-03 13:40:59 +0000

    Documentation for init.

--- init/init.c
+++ init/init.c
@@ -1,5 +1,11 @@
 #include "init.h"
 
+/**
+ * Initialization process upon startup of unix shell.
+ *
+ * @param env Stores PATH and HOME values.
+ * @return
+ */
 void init(EnvVariable** env) {
   read_env_val("profile", &(*env));
 }
